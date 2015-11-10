fbd21449cb2ad7bc7e0b0a48d2c2250668ba3052
Jorge Salvo <jorge.salvo@hpe.com>
2015-11-10 10:33:17 +0000

    Removing Hello statement

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,6 +3,5 @@ public class JavaHelloWorld
 	public static void main(String [] args){
 		System.out.println("Java Hello World");
 		System.out.println("Hello again");
-		System.out.println("Another Hello");
 	}
 }
